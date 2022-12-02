3c190d3e0689452eee6fd7937a334686f7530033
djk01281 <djk01281@gmail.com>
2022-12-02 15:52:20 +0000

    init: firebase demo

---
+++ public/app.js
@@ -0,0 +1,17 @@
+const temp = document.createElement("div");
+temp.innerHTML = "First Firebase App";
+document.querySelector("body").appendChild(temp);
+
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById("whenSignedIn");
+const whenSignedOut = document.getElementById("whenSignedOut");
+
+const signInBtn = document.getElementById("signInBtn");
+const signOutBtn = document.getElementById("signOutBtn");
+
+const userDetails = document.getElementById("userDetails");
+
+const provider = new firebase.auth.GoogleAuthProvider();
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+signOutBtn.onclick = () => auth.signOut();
